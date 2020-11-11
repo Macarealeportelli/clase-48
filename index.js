4abcd79befaa9b66114bd82a2897f496425a008d
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-11-11 19:18:45 -0300

    armando funcionalidad de boton buscar matches

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const grilla = document.querySelector('.grilla')
 const nuevoJuego = document.getElementById('nuevo-juego')
 const reiniciarJuego = document.getElementById('reiniciar-juego')
+const buscarMatches = document.getElementById('buscar-matches')
 
 
 const bienvenida = alert('Bienvenid@')
@@ -8,6 +9,9 @@ let seleccionaDificultad = prompt('Seleccione una dificultad: facil, medio, difi
 
 
 const frutas = ['🍉', '🍐', '🍌', '🍇', '🍎', '🍊']
+let grillaDeFrutas = []
+
+//Generación de Grilla dependiendo la dificultad
 
 const obtenerNumeroAlAzar = (items) => {
     return Math.floor((Math.random() * items.length))
@@ -20,14 +24,12 @@ const generarGrilla = (filas, columnas, items) => {
 
     grilla.innerHTML = ''
 
-    let grillaDeFrutas = []
-
     for (let i = 0; i < filas; i++) {
         grillaDeFrutas[i] = []
-        
+
         for (let j = 0; j < columnas; j++) {
             grillaDeFrutas[i][j] = items[obtenerNumeroAlAzar(items)]
-            grilla.innerHTML += `<div>${grillaDeFrutas[i][j]}</div>`
+            grilla.innerHTML += `<div data-x=${i} data-y=${j}>${grillaDeFrutas[i][j]}</div>`
         }
 
     }
@@ -38,30 +40,63 @@ const generarGrilla = (filas, columnas, items) => {
 
 // console.log(generarGrilla(6, 6, frutas))
 
-const mostrarGrilla=()=>{
+const mostrarGrilla = () => {
 
-    if (seleccionaDificultad === 'facil'){
-        generarGrilla(10,10,frutas)
+    if (seleccionaDificultad === 'facil') {
+        generarGrilla(10, 10, frutas)
     }
-    else if (seleccionaDificultad === 'medio'){
-        generarGrilla(8,8,frutas)
+    else if (seleccionaDificultad === 'medio') {
+        generarGrilla(8, 8, frutas)
+    }
+    else if (seleccionaDificultad === 'dificil'){
+        generarGrilla(6, 6, frutas)
     }
     else {
-        generarGrilla(6,6,frutas)
+        alert('No seleccionaste una opción correcta :(')
     }
 
 }
 
 mostrarGrilla()
 
-nuevoJuego.onclick = () =>{
+//Nuevo Juego
 
-   seleccionaDificultad = prompt('Seleccione una dificultad: facil, medio, dificil')
- 
+nuevoJuego.onclick = () => {
+
+    seleccionaDificultad = prompt('Seleccione una dificultad: facil, medio, dificil')
+
     mostrarGrilla()
 
 }
 
-reiniciarJuego.onclick=()=>{
+reiniciarJuego.onclick = () => {
     mostrarGrilla()
+}
+
+//Buscar Matches
+
+buscarMatches.onclick=()=>{
+
+    for (let i = 0; i < grillaDeFrutas.length; i++) {
+        for (let j = 0; j < grillaDeFrutas[i].length; j++) {
+          
+            if (grillaDeFrutas[i][j] === grillaDeFrutas[i][j+1] && grillaDeFrutas[i][j]===grillaDeFrutas[i][j+2]){
+                console.log(grillaDeFrutas[i][j], grillaDeFrutas[i][j+1], grillaDeFrutas[i][j+2])
+            }
+            
+        }
+        
+    }
+
+    for (let i = 0; i < grillaDeFrutas.length; i++) {
+        for (let j = 0; j < grillaDeFrutas[i].length; j++) {
+          
+            if (grillaDeFrutas[i][j] === grillaDeFrutas[i+1][j] && grillaDeFrutas[i+2][j]===grillaDeFrutas[i][j]){
+                console.log(grillaDeFrutas[i][j], grillaDeFrutas[i+1][j], grillaDeFrutas[i+2][j])
+            }
+            
+        }
+        
+    }
+
 }
